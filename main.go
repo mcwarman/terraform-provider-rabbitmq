10158f8827b876d5241b4552cbc1828cd35e04e8
Matthew Warman <mcwarman@gmail.com>
2023-08-21 08:37:20 +0000

    feat: add provider debug flag

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/terraform-providers/terraform-provider-rabbitmq/rabbitmq"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		Debug:        debug,
+		ProviderAddr: "registry.terraform.io/cyrilgdn/rabbitmq",
 		ProviderFunc: rabbitmq.Provider,
-	})
+	}
+
+	plugin.Serve(opts)
 }
